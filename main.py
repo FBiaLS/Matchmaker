89801fc93aee0a73209ea546497d1f9aaa3f6555
FBiaLS <fprockter9@gmail.com>
2024-10-12 15:13:15 +0100

    corrected loop length

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ groupTwo = [["nameone", "perferenceone", "perferencetwo", "perferencethree", "pe
 
 print("Welcome to Online Matchmaker! Input the names of 4 people who you want to find a match! (these people will not match with each other)")
 
-for i in range (3):
+for i in range (4):
     groupOne[i][0] = input()
 print("Now input the names of 4 different people for your initial 4 people to match with!")
-for i in range (3):
+for i in range (4):
     groupTwo[i][0] = input()
 print("Now its time to input preferences!")
-for i in range (3):
+for i in range (4):
     groupOne[i][1] = input("Who does group", groupOne[i][0], "prefer out of:", groupTwo[0][0], groupTwo[1][0], groupTwo[2][0], groupTwo[3][0] + "? ")
     groupOne[i][2] = input("Who is their second choice? ")
     groupOne[i][3] = input("Who is their third choice? ")
     groupOne[i][4] = input("Who is their fourth choice? ")
-for i in range (3):
+for i in range (4):
     groupTwo[i][1] = input("Who does group", groupTwo[i][0], "prefer out of:", groupOne[0][0], groupOne[1][0], groupOne[2][0], groupOne[3][0] + "? ")
     groupTwo[i][2] = input("Who is their second choice? ")
     groupTwo[i][3] = input("Who is their third choice? ")
